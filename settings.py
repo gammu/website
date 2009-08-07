783e2975b0d69fa6c609c0d44b636ec028081d86
Michal Čihař <mcihar@novell.com>
2009-08-07 14:22:03 +0200

    Add some context processors.

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
-# Django settings for wammu project.
+# Django settings for wammu website project.
+# -*- coding: UTF-8 -*-
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -65,6 +66,14 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'wammu_web.urls'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    )
 
 TEMPLATE_DIRS = (
     '/home/mcihar/private/wammu_web/html/',
