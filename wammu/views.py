a1df4e108aea5c8ba83b7b3b4adca8817b8882bc
Michal Čihař <mcihar@novell.com>
2009-08-07 15:25:42 +0200

    Actually start with some content.

--- wammu/views.py
+++ wammu/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render_to_response
 from wammu_web.wammu.helpers import WammuContext
+from wammu_web.news.models import Entry
 
 # Create your views here.
 
 def index(request):
+    news = Entry.objects.order_by('-pub_date')[:5]
     return render_to_response('index.html', WammuContext(request, {
-        'a': 'b'
+        'news': news,
     }))
