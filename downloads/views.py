2311be01e108c074499041d617b83a4dc84adfb5
Michal Čihař <michal@cihar.com>
2009-08-21 22:39:45 +0200

    Add mirror selection.

--- downloads/views.py
+++ downloads/views.py
@@ -2,15 +2,34 @@ from django.shortcuts import render_to_response, get_object_or_404
 from wammu_web.wammu.helpers import WammuContext
 from downloads.models import Download, Mirror, get_program
 from django.http import Http404
+from django.utils.datastructures import MultiValueDictKeyError
 
 def list(request, program, platform):
     downloads = Download.objects.filter(program = program, platform = platform).order_by('location')
+    mirrors = Mirror.objects.all().order_by('id')
+    set_mirror = False
+    try:
+        try:
+            mirror_id = request.GET['mirror']
+        except MultiValueDictKeyError:
+            mirror_id = request.COOKIES['mirror']
+        set_mirror = True
+    except MultiValueDictKeyError:
+        mirror_id = 'cihar-com'
+    try:
+        mirror = Mirror.objects.get(slug = mirror_id)
+    except Mirror.DoesNotExist:
+        mirror = Mirror.objects.get(slug = 'cihar-com')
     if downloads.count() == 0:
         raise Http404('No such download option %s/%s.' % (program, platform))
 
-    return render_to_response('downloads/list.html', WammuContext(request, {
+    result = render_to_response('downloads/list.html', WammuContext(request, {
         'downloads': downloads,
         'program_include': 'downloads/programs/%s-%s.html' % (program, platform),
         'program': get_program(program),
+        'mirrors': mirrors,
+        'mirror': mirror,
     }))
-
+    if set_mirror:
+        result.set_cookie('mirror', mirror_id, max_age = 3600 * 24 * 365)
+    return result
