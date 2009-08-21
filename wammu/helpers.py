e78b66312d08d60ee96275948cc6304510127c6c
Michal Čihař <mcihar@novell.com>
2009-08-21 16:10:03 +0200

    Synchronise top menu with bottom navigation.

--- wammu/helpers.py
+++ wammu/helpers.py
@@ -7,10 +7,10 @@ class WammuContext(RequestContext):
         context['current_year'] = datetime.now().strftime('%Y')
         context['generated'] = datetime.now().strftime('%a, %d %b %Y %H:%M:%S GMT')
         context['menu'] = [
-            {'title': 'About', 'link': '/'},
+            {'title': 'Support', 'link': '/support/'},
             {'title': 'Download', 'link': '/download/'},
+            {'title': 'Documentation', 'link': '/docs/'},
             {'title': 'Contribute', 'link': '/contribute/'},
-            {'title': 'Documentation', 'link': '/documentation/'},
             ]
         RequestContext.__init__(self, request, context)
 
