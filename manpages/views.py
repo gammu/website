467c00ae3505a69e14431f221cba75023c8ca0cc
Michal Čihař <michal@cihar.com>
2009-09-04 15:31:27 +0200

    Fix propagating of page  title.

--- manpages/views.py
+++ manpages/views.py
@@ -6,5 +6,5 @@ def show_page(request, page, lang = 'en'):
     manpage = 'docs/man/%s/%s.html' % (lang, page)
     return render_to_response('docs/show_man.html', WammuContext(request, {
         'manpage': manpage,
-        'title': page,
+        'page': page,
     }))
