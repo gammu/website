e8e35f1a7f09a84788a0f6457b856a686272b9a6
Michal Čihař <michal@cihar.com>
2009-09-09 15:05:48 +0200

    Add navigation.

--- phonedb/models.py
+++ phonedb/models.py
@@ -35,6 +35,10 @@ class Vendor(models.Model):
     def __unicode__(self):
         return self.name
 
+    @models.permalink
+    def get_absolute_url(self):
+         return ('phonedb.views.vendor', (), {'vendorname': self.slug})
+
 class Feature(models.Model):
     name = models.CharField(max_length = 250)
 
