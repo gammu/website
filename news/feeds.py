a0f47bcab5ae162e5fbf32b723afbbd624d860fb
Michal Čihař <michal@cihar.com>
2009-09-16 10:55:59 +0200

    Configurable number of news in RSS.

--- news/feeds.py
+++ news/feeds.py
@@ -3,6 +3,7 @@ from django.contrib.syndication.feeds import Feed
 from news.models import Entry, Category
 from django.utils.feedgenerator import Atom1Feed
 from django.utils.translation import ugettext as _
+from django.conf import settings
 
 class RssNewsFeed(Feed):
     title = _('Wammu and Gammu News')
@@ -15,7 +16,7 @@ class RssNewsFeed(Feed):
         print [x.title for x in Category.objects.all()]
 
     def items(self):
-        return Entry.objects.order_by('-pub_date')[:5]
+        return Entry.objects.order_by('-pub_date')[:settings.NEWS_IN_RSS]
 
     def item_author_name(self, obj):
         return obj.author.get_full_name()
