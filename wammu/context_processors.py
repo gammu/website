de800c49657a597cec286cbe5e30ee9189a5a843
Michal Čihař <michal@cihar.com>
2009-12-05 15:13:23 +0100

    Add link to page translations.
    Each page now includes link to translation(s) in sidebar.

---
+++ wammu/context_processors.py
@@ -0,0 +1,16 @@
+# Context processors
+# -*- coding: UTF-8 -*-
+
+from django.conf import settings
+
+def translations(request):
+    path = request.get_full_path()
+    langs = []
+
+    if settings.LANGUAGE_CODE != 'cs-cz':
+        langs.append({'url': 'http://cs.wammu.eu%s' % path, 'name': u'Česky'})
+
+    if settings.LANGUAGE_CODE != 'en-us':
+        langs.append({'url': 'http://wammu.eu%s' % path, 'name': u'English'})
+
+    return {'translations': langs}
