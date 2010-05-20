957efed59832797b5e9404344f05436fbdaf9bc6
Michal Čihař <michal@cihar.com>
2010-05-20 15:16:35 +0200

    Translatable chart.

--- phonedb/views.py
+++ phonedb/views.py
@@ -95,7 +95,7 @@ def get_chart_url():
 # Set the horizontal dotted lines
     chart.set_grid(0, 10, 5, 5)
 
-    chart.set_legend(['Supported phones', 'Valid records', 'Total records'])
+    chart.set_legend([_('Supported phones'), _('Valid records'), _('Total records')])
 
     left_axis = map(lambda x: '%d' % x, xrange(0, max_y + 1, max_y / 10))
     left_axis[0] = ''
