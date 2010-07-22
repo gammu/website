d17783d028d32f6783d5e4a14a95666ea29b7e02
Michal Čihař <michal@cihar.com>
2010-07-22 17:37:26 +0200

    Decode also message data.

--- tools/views.py
+++ tools/views.py
@@ -13,20 +13,33 @@ def pdudecode(request):
     if request.method == 'POST':
         form = PDUDecodeForm(request.POST)
         if form.is_valid():
+            # Decode PDU
             decoded = []
+            parts = []
             for i, part in enumerate(form.cleaned_data['text'].split()):
                 try:
                     d = gammu.DecodePDU(part.decode('hex'))
+                    parts.append([d])
                     d['TextHex'] = d['Text'].encode('hex')
                     d['UDH']['TextHex'] = d['UDH']['Text'].encode('hex')
+                    d['Id'] = i + 1
+                    d['PDU'] = part
                     decoded.append(d)
                 except gammu.GSMError, e:
                     decoded.append({'Error': e[0]})
+            # Link multipart messages
+            linked = gammu.LinkSMS(parts)
+            # Decode multipart messages
+            smsinfo = []
+            for i, x in enumerate(linked):
+                d = gammu.DecodeSMS(x)
                 d['Id'] = i + 1
-                d['PDU'] = part
+                smsinfo.append(d)
+
     else:
         form = PDUDecodeForm()
     return render_to_response('tools/pdudecode.html', RequestContext(request, {
         'form': form,
         'decoded': decoded,
+        'smsinfo': smsinfo,
     }))
