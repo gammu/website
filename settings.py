86b6ef7a8257e7969ac01af1da993fc66d8e981a
Michal Čihař <michal@cihar.com>
2011-03-31 11:23:32 +0200

    Adjust loaders config to Django 1.3

--- settings.py
+++ settings.py
@@ -68,10 +68,8 @@ SECRET_KEY = 'c=kt_6vtz&(418w-0(uti(q5&e76q#lc=%vuwzm&+ulqrkgyp3'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
