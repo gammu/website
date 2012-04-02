6cbe80a8150209c1b2088bed1c554c2252a9abfa
Michal Čihař <michal@cihar.com>
2012-04-02 10:45:34 +0200

    Really fix phonedb feeds

--- phonedb/feeds.py
+++ phonedb/feeds.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-from django.contrib.syndication.feeds import Feed
+from django.contrib.syndication.views import Feed
 from phonedb.models import Vendor, Phone
 from django.utils.feedgenerator import Atom1Feed
 from django.utils.translation import ugettext as _
