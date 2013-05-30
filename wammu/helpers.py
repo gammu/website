ac6de09d384b68f0397d9a5566903e5d7de3c82b
Michal Čihař <michal@cihar.com>
2013-05-30 14:43:20 +0200

    Support links to github

--- wammu/helpers.py
+++ wammu/helpers.py
@@ -1,6 +1,7 @@
 import re
 
 BUG_RE = re.compile('(bug ?#([0-9]*))')
+ISSUE_RE = re.compile('(issue ?#([0-9]*))')
 LP_RE = re.compile('(LP ?#([0-9]*))')
 BDO_RE = re.compile('(bdo ?#([0-9]*))')
 
@@ -10,4 +11,5 @@ def process_bug_links(text):
     '''
     text = LP_RE.sub(r'[\1](https://bugs.launchpad.net/bugs/\2)', text)
     text = BDO_RE.sub(r'[\1](http://bugs.debian.org/\2)', text)
+    text = ISSUE_RE.sub(r'[\1](https://github.com/gammu/gammu/\2)', text)
     return BUG_RE.sub(r'[\1](https://bugs.cihar.com/\2)', text)
