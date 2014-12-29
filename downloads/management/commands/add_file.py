6f9782621f3014aa17bd20a249c208e0592ff890
Michal Čihař <michal@cihar.com>
2014-12-29 14:45:26 +0100

    Turn add_file.py to management command
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ downloads/management/commands/add_file.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+import hashlib
+from downloads.models import Release, Download
+
+
+class Command(BaseCommand):
+    help = 'adds file to the release'
+
+    def handle(self, *args, **options):
+        if len(args) < 6:
+            print 'Usage: add_file /base/path program version type file...'
+            sys.exit(1)
+
+        release = Release.objects.get(program = args[2], version = args[3])
+
+        dlpath = args[1]
+
+        while dlpath[-1] == '/':
+            dlpath = dlpath[:-1]
+
+        for f in args[5:]:
+            print "Adding %s..." % f
+            path, filename = os.path.split(f)
+
+            data = open(f).read()
+
+            dl = Download()
+            dl.release = release
+
+            md5 = hashlib.md5()
+            md5.update(data)
+            dl.md5 = md5.hexdigest()
+
+            sha1 = hashlib.sha1()
+            sha1.update(data)
+            dl.sha1 = sha1.hexdigest()
+
+            dl.size = len(data)
+
+            dl.platform = args[4]
+
+            dl.location = '%s/%s' % (dlpath, filename)
+
+            dl.save()
