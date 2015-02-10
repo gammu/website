d35c86d563cd3237322c5500773bda109651440c
Michal Čihař <michal@cihar.com>
2015-02-10 15:42:34 +0100

    Fix setting pt-br
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- wammu/middleware.py
+++ wammu/middleware.py
@@ -7,7 +7,7 @@ class SiteLocaleMiddleware(object):
         current_site = get_current_site(request)
 
         lang = current_site.domain.split('.')[0]
-        if len(lang) == 2:
+        if len(lang) == 2 or len(lang) == 5:
             translation.activate(lang)
             request.LANGUAGE_CODE = translation.get_language()
         else:
