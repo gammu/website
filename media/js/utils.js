5e687bd7a22abc61d085e6b3242933e6ac756b70
Michal Čihař <michal@cihar.com>
2015-04-09 08:40:28 +0200

    Remove Flattr, it seems to be dead
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- media/js/utils.js
+++ media/js/utils.js
@@ -14,15 +14,3 @@ $(document).ready(function(){
     /* Hide submit button */
     $('form#mirrorselect input').addClass('jshidden');
 });
-
-/* Flattr */
-(function() {
-    var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-
-    s.type = 'text/javascript';
-    s.async = true;
-    s.src = 'http://api.flattr.com/js/0.5.0/load.js?mode=auto';
-
-    t.parentNode.insertBefore(s, t);
-})();
-
