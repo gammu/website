9f14425893b3f08ee6f18952d0c4039f18dd6671
Michal Čihař <michal@cihar.com>
2015-04-13 13:12:39 +0200

    Simplify getlink to always use default mirror
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- downloads/templatetags/getlink.py
+++ downloads/templatetags/getlink.py
@@ -1,23 +1,10 @@
 from django import template
+from downloads.models import Mirror
 
 register = template.Library()
 
-class GetLink(template.Node):
-    def __init__(self, dlname):
-        self.dlname = template.Variable(dlname)
-        self.mirror =  template.Variable('mirror')
 
-    def render(self, context):
-        mirror = self.mirror.resolve(context)
-        download = self.dlname.resolve(context)
-        return mirror.getlink(download)
-
-@register.tag
-def getlink(parser, token):
-    try:
-        # split_contents() knows not to split quoted strings.
-        tag_name, dlname = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError, "%r tag requires exactly one argument" % token.split_contents()[0]
-
-    return GetLink(dlname)
+@register.simple_tag
+def getlink(dlname):
+    mirror = Mirror.objects.get(slug='cihar-com')
+    return mirror.getlink(dlname)
