61a35c0d733cb848300f382d1498276e1cc59ec4
Michal Čihař <michal@cihar.com>
2016-09-09 09:19:52 +0200

    Use short language codes
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- wammu/context_processors.py
+++ wammu/context_processors.py
@@ -15,22 +15,22 @@ def translations(request):
     lang = get_language()
 
     if lang != 'cs':
-        langs.append({'url': 'https://cs.wammu.eu%s' % path, 'name': u'Česky', 'code': 'cs-CZ'})
+        langs.append({'url': 'https://cs.wammu.eu%s' % path, 'name': u'Česky', 'code': 'cs'})
 
     if lang != 'en':
-        langs.append({'url': 'https://wammu.eu%s' % path, 'name': u'English', 'code': 'en-US'})
+        langs.append({'url': 'https://wammu.eu%s' % path, 'name': u'English', 'code': 'en'})
 
     if lang != 'es':
-        langs.append({'url': 'https://es.wammu.eu%s' % path, 'name': u'Español', 'code': 'es-ES'})
+        langs.append({'url': 'https://es.wammu.eu%s' % path, 'name': u'Español', 'code': 'es'})
 
     if lang != 'de':
-        langs.append({'url': 'https://de.wammu.eu%s' % path, 'name': u'Deutsch', 'code': 'de-DE'})
+        langs.append({'url': 'https://de.wammu.eu%s' % path, 'name': u'Deutsch', 'code': 'de'})
 
     if lang != 'sk':
-        langs.append({'url': 'https://sk.wammu.eu%s' % path, 'name': u'Slovenčina', 'code': 'sk-SK'})
+        langs.append({'url': 'https://sk.wammu.eu%s' % path, 'name': u'Slovenčina', 'code': 'sk'})
 
     if lang != 'fr':
-        langs.append({'url': 'https://fr.wammu.eu%s' % path, 'name': u'Français', 'code': 'fr-FR'})
+        langs.append({'url': 'https://fr.wammu.eu%s' % path, 'name': u'Français', 'code': 'fr'})
 
     if lang != 'pt-br':
         langs.append({'url': 'https://pt-br.wammu.eu%s' % path, 'name': u'Português brasileiro', 'code': 'pt-BR'})
