21aeb52243373c1c3cb23ef66ea3165925822e99
Michal Čihař <michal@cihar.com>
2018-09-23 07:12:55 +0200

    Fix inclusion of admin urls
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- urls.py
+++ urls.py
@@ -186,7 +186,7 @@ urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 
     # Media files
     url(r'^media/(?P<path>.*)$', django.views.static.serve,
