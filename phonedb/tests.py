a6608fdd5cb949bae7fe6b6bb2d1a8f77b237a60
Michal Čihař <michal@cihar.com>
2018-09-23 07:21:09 +0200

    Avoid using deprecated django.core.urlresolvers
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- phonedb/tests.py
+++ phonedb/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.sites.models import Site
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.test import TestCase
 
 from phonedb.models import Vendor, Feature, Connection
