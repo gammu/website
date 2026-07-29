a21b885f56b357f10581568b136b94827070d977
Michal Čihař <michal@cihar.com>
2026-07-29 11:38:32 +0200

    fix: properly use static files

--- settings.py
+++ settings.py
@@ -70,7 +70,7 @@ MEDIA_URL = "/media/"
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-STATIC_ROOT = ""
+STATIC_ROOT = f"{WEB_ROOT}/static/"
 
 # URL prefix for static files.
 STATIC_URL = "/static/"
